fab21ba653685554ef46c48ccc4faf01e7c7b34a
Rabit <rabit2245@gmail.com>
2022-02-12 23:11:53 +0100

    created portfolio project but not finished yet

---
+++ src/data.js
@@ -0,0 +1,54 @@
+// src/data.js
+
+export const projects = [
+  {
+    title: 'React Reserve',
+    subtitle: 'MERN Stack',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?',
+    image: './project-1.gif',
+    link: 'https://www.linkedin.com/feed/',
+  },
+  {
+    title: 'React Tracks',
+    subtitle: 'React and Python',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?',
+    image: './project-2.gif',
+    link: 'https://www.twitch.tv/',
+  },
+  {
+    title: 'DevChat',
+    subtitle: 'React and Firebase',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?',
+    image: './project-3.gif',
+    link: 'https://reactjs.org/docs/getting-started.html',
+  },
+  {
+    title: 'Epic Todo App',
+    subtitle: 'React Hooks',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?',
+    image: './project-4.gif',
+    link: 'https://www.freecodecamp.org/learn/',
+  },
+];
+export const testimonials = [
+  {
+    company: 'React Reserve',
+    name: 'MERN Stack',
+    quote:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?',
+    image: 'Mern.jpg',
+  },
+  {
+    company: 'React Tracks',
+    name: 'React and Python',
+    quote:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?',
+    image: 'logo512.png',
+  },
+];
+
+export const skills = ['React', 'Node', 'JavaScript', 'Firebase', 'MongoDB'];
