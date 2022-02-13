98eac4413c403b71567cd9a9d3b6c74bbcae1736
Rabit <rabit2245@gmail.com>
2022-02-13 22:27:28 +0100

    styling changes

--- src/components/About.js
+++ src/components/About.js
@@ -6,7 +6,7 @@ export default function About() {
   return (
     <section id='about'>
       <div className='container mx-auto flex px-10 py-20 md:flex-row flex-col items-center'>
-        <div className='lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center'>
+        <div className='lg:flex-grow md:w-1/2 lg:pr-24 md:pr-8  flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center '>
           <h1 className='title-font sm:text-4xl text-3xl mb-4 font-medium text-white'>
             Hi, I'm Reed.
             <br className='hidden lg:inline-block' />I love to build amazing
@@ -20,7 +20,7 @@ export default function About() {
           <div className='flex justify-center'>
             <a
               href='#contact'
-              className='inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg'
+              className='inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600  rounded text-lg'
             >
               Work With Me
             </a>
@@ -32,12 +32,11 @@ export default function About() {
             </a>
           </div>
         </div>
-        <div className='lg:max-w-lg lg:w-full md:w-1/2 w-5/6'>
+        <div className='lg:max-w-lg lg:w-full md:w-1/2 w-5/6 '>
           <img
-            className='object-cover object-center rounded'
+            className='object-cover object-center rounded mx-auto md:mr-0'
             alt='hero'
             src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_nIB0iN1jMkdAgMeId0X8BRtRRcKZ5ktpeA&usqp=CAU'
-            
           />
         </div>
       </div>
