42b4b0fa3ef359158498b18fbeeccc59d3993463
Rabit <rabit2245@gmail.com>
2022-02-13 22:29:36 +0100

    minor change

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,7 +12,7 @@ export default function Navbar() {
           className='title-font font-medium text-white mb-4 md:mb-0'
         >
           <a href='#about' className='ml-3 text-xl'>
-            Reed Barger
+            Rabit Ebibi
           </a>
         </a>
         <nav className='md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center'>
